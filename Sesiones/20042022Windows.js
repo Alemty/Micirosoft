bbb360eb969279c19f7573b75a61cc5225ecf624
Alejandro Gutierrez <99059511+Alemty@users.noreply.github.com>
2022-04-20 11:27:13 -0500

    Create 20042022Windows.js

---
+++ Sesiones/20042022Windows.js
@@ -0,0 +1,11 @@
+**Windows 111**
+
+** Por qué Windows?**
+- 1.5 Billones de dispositivos
+- 345 millones obsoletos (W7 W10 W11)
+
+**Por qué lso clientes no migran a W11?**
+- 38% Usan Windows10
+- 22% quieren esperar a ver que tan seguro es
+- 22% no están seguros si su equipo cumple con las caracteristicas
+- 29% no lo conocen lo suficiente
